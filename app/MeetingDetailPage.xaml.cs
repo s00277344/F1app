ee0c8b505567aefc46d1983f9ac92c9589cac973
Yoann <yoann.silvain@gmail.com>
2025-03-26 17:26:27 +0000

    meeting detail page done

--- app/MeetingDetailPage.xaml.cs
+++ app/MeetingDetailPage.xaml.cs
@@ -1,10 +1,12 @@
 using Newtonsoft.Json;
 using OpenF1CSharp;
+using System.Diagnostics;
 
 namespace app;
 
 public partial class MeetingDetailPage : ContentPage
 {
+	int MeetingKey = 1255;
 	public MeetingDetailPage()
 	{
 		InitializeComponent();
@@ -15,11 +17,29 @@ public partial class MeetingDetailPage : ContentPage
 		OpenF1Reader openF1Reader = new OpenF1Reader();
 
 		var rawData = await openF1Reader.Query(new SessionQuery()
-			.Filter(nameof(SessionData.MeetingKey), 1255)
+			.Filter(nameof(SessionData.MeetingKey), MeetingKey)
 			.GenerateQuery());
 
-		if (rawData is null) return;
+		if (rawData is null)
+		{
+			Debug.WriteLine("raw data empty");
+			return;
+		}
 		var Data = JsonConvert.DeserializeObject<List<SessionData>>(rawData);
 		Sessions.ItemsSource = Data;
+
+		
+		rawData = await openF1Reader.Query(new MeetingQuery()
+			.Filter(nameof(MeetingData.MeetingKey), MeetingKey)
+			.GenerateQuery());
+
+		if (rawData is null)
+		{
+            Debug.WriteLine("raw data empty");
+            return;
+        }
+		var data = JsonConvert.DeserializeObject<List<MeetingData>>(rawData);
+		if (data is null) return;
+		NameLabel.Text = $"{data.First().MeetingName} Schedule";
     }
 }
